99e4e2e29642b42356cc83c33644dd5d6d373ba6
Andrei <leonichev.design@gmail.com>
2022-05-16 21:38:46 +0000

    Before review mode

--- client/src/components/common/popup-add.tsx
+++ client/src/components/common/popup-add.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
-import { SearchInput, GreyButton } from "../styled/common";
-import { BodyTable, OrderTableTh, OrderColumnTh, OCThInside, OrderTableItem, OrderColumn, OCInside, OrdImage, StatusBlock, Status, StatusMess, StatusOK, StatusOKIcon, StatusX, StatusXIcon, StatusEdit, StatusEditLink } from "../styled/order-body";
-import { PopTop, PopClose, PopCloseIcon, PopBottom, PopTopTitle, PopLine, PopLineIn, PopMiddleAdd, PopSearchBlock, PopSearchTitle, PopSearchEmpty, PopSearchEmptyIcon, PopSearchResults, PopInput } from "../styled/popup";
+import React, { useEffect, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { getIDKeysArray } from "../../utill/get-arrays";
+import { inputPriceAddPopup, inputQuantityAddPopup } from "../../utill/input-checks";
+import { searchFilterCheck } from "../../utill/search-utills";
+import { SearchInput, GreyButton, Bold } from "../styled/common";
+import { BodyTable, OrderTableTh, OrderColumnTh, OCThInside, OrderTableItem, OrderColumn, OCInside, OrdImage } from "../styled/order-body";
+import { PopTop, PopClose, PopCloseIcon, PopTopTitle, PopLine, PopLineIn, PopMiddleAdd, PopSearchBlock, PopSearchTitle, PopSearchEmpty, PopSearchEmptyIcon, PopSearchResults, PopInput, PopBottomCount, PopBottomAdd } from "../styled/popup";
 import PopupCommon from "./popup-common";
 
 type PopupEditProps = {
@@ -13,7 +18,56 @@ export default function PopupAdd(props: PopupEditProps) {
 
     const { show, closeHandler } = props;
 
-    const [searchValue, setSearchValue] = useState("");
+    const orderID = useSelector((state: RootState) => state.order.currentOrder.id);
+    const products = useSelector((state: RootState) => state.products.productsArr);
+    const productsById = useMemo(() => getIDKeysArray(products), [products]);
+    const [searchInp, setsearchInp] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [priceInp, setPriceInp] = useState(new Array());
+    const [quantityInp, setQuantityInp] = useState(new Array());
+    const [selectedProdIds, setSelectedProdIds] = useState(new Array());
+
+    useEffect(() => {
+        if (quantityInp.length > 2) {
+            const selectedProdIdsPre = quantityInp.filter((elm) => {
+                if (elm !== undefined) {
+                    return elm.value !== "";
+                }
+                return false;
+            });
+            setSelectedProdIds(selectedProdIdsPre);
+        }
+    }, [quantityInp])
+
+    useEffect(() => {
+        if (products.length > 0) {
+            const priceInpPre = getIDKeysArray(products.map((elm: any) => {
+                return {
+                    "id": elm.id,
+                    "value": "$22"
+                }
+            }));
+            setPriceInp(priceInpPre);
+            const quantityInpPre = getIDKeysArray(products.map((elm: any) => {
+                return {
+                    "id": elm.id,
+                    "value": ""
+                }
+            }));
+            setQuantityInp(quantityInpPre);
+        }
+    }, [products]);
+
+    useEffect(() => {
+        if (products.length > 0) {
+            const searchResultsPre = products.filter((elm: any) => {
+                return searchInp !== ""
+                    ? searchFilterCheck(searchInp, elm.name)
+                    : false;
+            });
+            setSearchResults(searchResultsPre);
+        }
+    }, [orderID, products, searchInp]);
 
     return (
         <PopupCommon size={70} show={show} closeHandler={closeHandler}>
@@ -29,9 +83,9 @@ export default function PopupAdd(props: PopupEditProps) {
             <PopMiddleAdd>
                 <PopSearchTitle>Search products from Sysco's catalog and add quantity</PopSearchTitle>
                 <PopSearchBlock>
-                    <SearchInput value={searchValue} onInput={(e) => setSearchValue(e.currentTarget.value)} size={50} placeholder="Search..." />
+                    <SearchInput value={searchInp} onInput={(e) => setsearchInp(e.currentTarget.value)} size={50} placeholder="Search..." />
                 </PopSearchBlock>
-                {(searchValue.length === 0)
+                {(searchResults.length === 0)
                     ?
                     <PopSearchEmpty>
                         <PopSearchEmptyIcon size={155} />
@@ -47,37 +101,46 @@ export default function PopupAdd(props: PopupEditProps) {
                                 <OrderColumnTh><OCThInside corner="">Price</OCThInside></OrderColumnTh>
                                 <OrderColumnTh><OCThInside corner="right">Qt.</OCThInside></OrderColumnTh>
                             </OrderTableTh>
-                            {[1, 2, 3, 4, 5, 6].map((elm, i) => {
-                                return (
-                                    <OrderTableItem>
-                                        <OrderColumn>
-                                            <OCInside side="left" corner={i === 5 ? "left" : ""}>
-                                                <OrdImage src="/assets/images/avocado.jpg" />
-                                            </OCInside>
-                                        </OrderColumn>
-                                        <OrderColumn><OCInside side="" corner="">Chicken Breast Fillets, Boneless Marinated 6 Ounce Raw, Invivid</OCInside></OrderColumn>
-                                        <OrderColumn><OCInside side="" corner="">Hormel Black Labelmany</OCInside></OrderColumn>
-                                        <OrderColumn><OCInside side="" corner="">10/32 OZ</OCInside></OrderColumn>
-                                        <OrderColumn>
-                                            <OCInside side="" corner="">
-                                                <PopInput size={75} />
-                                            </OCInside>
-                                        </OrderColumn>
-                                        <OrderColumn>
-                                            <OCInside side="right" corner={i === 5 ? "right" : ""}>
-                                                <PopInput size={75} value="4" />
-                                            </OCInside>
-                                        </OrderColumn>
-                                    </OrderTableItem>
-                                );
-                            })}
+                            {searchResults.length > 0 &&
+                                productsById.length > 0 &&
+                                searchResults.map((elm: any, i: number) => {
+                                    return (
+                                        <OrderTableItem>
+                                            <OrderColumn>
+                                                <OCInside side="left" corner={i === searchResults.length - 1 ? "left" : ""}>
+                                                    <OrdImage src={`/assets/images/${elm.image}`} />
+                                                </OCInside>
+                                            </OrderColumn>
+                                            <OrderColumn><OCInside side="" corner="">{elm.name}</OCInside></OrderColumn>
+                                            <OrderColumn><OCInside side="" corner="">{elm.brand}</OCInside></OrderColumn>
+                                            <OrderColumn><OCInside side="" corner="">Default</OCInside></OrderColumn>
+                                            <OrderColumn>
+                                                <OCInside side="" corner="">
+                                                    <PopInput size={75} value={priceInp[elm.id].value} onInput={(e) => {
+                                                        inputPriceAddPopup(e.currentTarget.value, priceInp, elm.id, setPriceInp);
+                                                    }} />
+                                                </OCInside>
+                                            </OrderColumn>
+                                            <OrderColumn>
+                                                <OCInside side="right" corner={i === searchResults.length - 1 ? "right" : ""}>
+                                                    <PopInput size={75} value={quantityInp[elm.id].value} onInput={(e) => {
+                                                        inputQuantityAddPopup(e.currentTarget.value, quantityInp, elm.id, setQuantityInp);
+                                                    }} />
+                                                </OCInside>
+                                            </OrderColumn>
+                                        </OrderTableItem>
+                                    );
+                                })}
                         </BodyTable>
                     </PopSearchResults>
                 }
             </PopMiddleAdd>
-            <PopBottom>
-                <GreyButton active={false}>Review</GreyButton>
-            </PopBottom>
+            <PopBottomAdd>
+                <PopBottomCount>
+                    {selectedProdIds.length > 0 && <><Bold>Total</Bold> {selectedProdIds.length} product{selectedProdIds.length > 1 && "s"}</>}
+                </PopBottomCount>
+                <GreyButton active={selectedProdIds.length > 0}>Review</GreyButton>
+            </PopBottomAdd>
         </PopupCommon>
     );
 }
